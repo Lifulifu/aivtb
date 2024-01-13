b0e799db6d268683bb09ff85effc961a3ecb5862
Lifulifu <andycscsmaple@gmail.com>
2024-01-13 10:12:00 +0800

    refactor

--- server/utils/pipeline.py
+++ server/utils/pipeline.py
@@ -1,4 +1,4 @@
-import threading
+from threading import Thread, Event
 from typing import Callable, List
 from dataclasses import dataclass
 import types
@@ -12,20 +12,20 @@ class PipelineStage:
     pre_process_delay: float = 0
     post_process_delay: float = 0
 
-class Worker(threading.Thread):
+class Worker(Thread):
     def __init__(self, stage: PipelineStage, in_queue: Queue, out_queue: Queue, event_manager: EventManager = None):
         super().__init__(daemon=True)
         self.in_queue = in_queue
         self.out_queue = out_queue
         self.stage = stage
-        self.events = event_manager
+        self.event_manager = event_manager
 
     def run(self):
         while True:
             task = self.in_queue.get()
             if task is None: break
-            if self.events:
-                self.events.publish(f'{self.stage.run.__name__}:start', task)
+            if self.event_manager:
+                self.event_manager.publish(f'{self.stage.run.__name__}:start', task)
 
             if self.stage.pre_process_delay > 0: time.sleep(self.stage.pre_process_delay)
             try:
@@ -38,15 +38,15 @@ class Worker(threading.Thread):
             if isinstance(result, types.GeneratorType):
                 for item in result:
                     self.out_queue.put(item)
-                    if self.events:
-                        self.events.publish(f'{self.stage.run.__name__}:end', task, item)
+                    if self.event_manager:
+                        self.event_manager.publish(f'{self.stage.run.__name__}:end', task, item)
             elif result is not None:
                 self.out_queue.put(result)
-                if self.events:
-                    self.events.publish(f'{self.stage.run.__name__}:end', task, result)
-            else: # abort this task by not putting anything into the next queue
-                if self.events:
-                    self.events.publish(f'{self.stage.run.__name__}:abort', task)
+                if self.event_manager:
+                    self.event_manager.publish(f'{self.stage.run.__name__}:end', task, result)
+            else: # result is None, abort this task by not putting anything into the next queue
+                if self.event_manager:
+                    self.event_manager.publish(f'{self.stage.run.__name__}:abort', task)
 
             self.in_queue.task_done()
 
@@ -56,7 +56,9 @@ class Pipeline():
         self.in_queue = in_queue if in_queue is not None else Queue()
         self.out_queue = out_queue if out_queue is not None else Queue()
         self.workers: List[Worker] = []
-        self.events = EventManager()
+        self.event_manager = EventManager()
+        self._thread_event = Event()
+
         self._create_pipeline(stages)
 
     def _create_pipeline(self, stages: List[Callable]):
@@ -67,7 +69,7 @@ class Pipeline():
                 next_queue = self.out_queue
             else:
                 next_queue = Queue()
-            worker = Worker(stage, queue, next_queue, event_manager=self.events)
+            worker = Worker(stage, queue, next_queue, event_manager=self.event_manager, thread_event=self._thread_event)
             self.workers.append(worker)
             queue = next_queue
 
@@ -81,6 +83,14 @@ class Pipeline():
     def end(self):
         for worker in self.workers:
             worker.join()
+
+    def abort(self):
+        '''abort all unfinished tasks'''
+        while not self.in_queue.empty():
+            self.in_queue.get()
+        while not self.out_queue.empty():
+            self.out_queue.get()
+
 
 if __name__ == '__main__':
     def input_generator():
@@ -104,8 +114,8 @@ if __name__ == '__main__':
         PipelineStage(run=func2, post_process_delay=1),
     ]
     pipeline = Pipeline(stages, in_queue)
-    pipeline.events.subscribe('func1:start', lambda task: print(f'func1:start {task}'))
-    pipeline.events.subscribe('func2:start', lambda task: print(f'func2:start {task}'))
+    pipeline.event_manager.subscribe('func1:start', lambda task: print(f'func1:start {task}'))
+    pipeline.event_manager.subscribe('func2:start', lambda task: print(f'func2:start {task}'))
     pipeline.start()
 
     for task in input_generator():
